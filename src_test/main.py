3bd8828369dd31da867972527015342c7627f73e
michael.brewster <michael.brewster@ibm.com>
2019-02-10 14:21:39 +0000

    updates

--- src_test/main.py
+++ src_test/main.py
@@ -1,1 +1,13 @@
+import collections
+import math
+
 print("Hello World")
+
+
+counter = collections.Counter()
+
+for i in range(0, 100000):
+    counter[math.sqrt(i) // 25] += 1
+
+for key, count in counter.most_common(5):
+    print('Count for %s: %d' % (key, count))
