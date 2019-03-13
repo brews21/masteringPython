c0f1fa2734fc161229794815eab24819e00bdbc5
michael.brewster <michael.brewster@ibm.com>
2019-03-13 21:06:50 +0000

    stuff

--- modernPythonCookbook/Chapter08/main.py
+++ modernPythonCookbook/Chapter08/main.py
@@ -6,7 +6,17 @@ the python file is use for/ meant to do
 '''
 
 import sys
-print "This is the name of the script: ", sys.argv[0]
-print "Number of arguments: ", len(sys.argv)
-print "The arguments are: " , str(sys.argv)
-print "this is: ", sys.argv[1]
+#print "This is the name of the script: ", sys.argv[0]
+#print "Number of arguments: ", len(sys.argv)
+#print "The arguments are: " , str(sys.argv)
+#print "this is: ", sys.argv[1]
+
+import random
+
+random.seed()
+
+my_list = ["a", "b", "c"]
+
+d = my_list[random.randrange(len(my_list))]
+
+print(d)
