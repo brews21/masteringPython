8e6f1a851f92e1484c1e7bdee7613c374c27e36b
michael.brewster <michael.brewster@ibm.com>
2019-09-27 18:41:05 +0100

    updates

--- myTest.py
+++ myTest.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
+import logging
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
 
 a = 1
 
 def myFunction(val):
     global a
     a = val
-    print(a)
+    logging.info(a)
     return a
 
 
-print(a)
-myFunction(2)
-print(a)
+logging.debug(a)
+logging.debug(myFunction(2))
+logging.debug(a)
